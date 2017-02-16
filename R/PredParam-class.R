fe6b50fde8ab93a802ee3a50fe1d612423570538
david-salgado <david.salgado.fernandez@gmail.com>
2017-02-16 16:47:09 +0100

    Define classes PredParam and PredlmParam

---
+++ R/PredParam-class.R
@@ -0,0 +1,8 @@
+setClassUnion('PredParam', c('PredlmParam'))
+#' @title Virtual S4 class to handle parameters for the estimation of the probability of measurement error
+#'
+#' @name PredParam
+#'
+#' @include PredlmParam-class.R
+#'
+NULL
---
+++ R/PredlmParam-class.R
@@ -0,0 +1,62 @@
+#' @title S4 class for the parameters for prediction based on a linear model
+#'
+#' @description Definition of the S4 class named \code{PredlmParam} for the parameters to make
+#' predictions in the optimization approach to selective editing.
+#'
+#'
+#' @slot RawData \linkS4class{StQ} object with the raw data.
+#'
+#' @slot EdData \linkS4class{StQ} object with the edited data.
+#'
+#' @slot VarNames Character vector with the names of the variables whose probability errors are to
+#' be computed.
+#'
+#' @slot DomainNames Character vector with the names of the variables determining the domains of the
+#' population within which predictions are to be calculated separately.
+#'
+#' @slot Imputation \linkS4class{ImputationParam} object with the parameters to imputed missing
+#' values during the computation of predicted values and their standard deviations (predition error
+#' standard deviations).
+#'
+#' @examples
+#' # An empty PredlmParam object:
+#' new(Class = 'PredlmParam')
+#'
+#' \dontrun{
+#' ImpParam <- new(Class = 'PredlmParam',
+#'                 VarNames = 'CifraNeg_13.___',
+#'                 DomainNames =  'Tame_05._4.')
+#' ErrorProbMLEParam <- new(Class = 'PredlmParam',
+#'                          RawData = FD.StQList,
+#'                          EdData = FG.StQList,
+#'                          VarNames = 'CifraNeg_13.___',
+#'                          Imputation = ImpParam)
+#'
+#'
+#' }
+#'
+#' @import data.table StQ RepoTime StQImputation
+#'
+#' @export
+setClass(Class = "PredlmParam",
+         slots = c(RawData = 'StQList',
+                   EdData = 'StQList',
+                   VarNames = 'character',
+                   DomainNames = 'character',
+                   Imputation = 'ImputationParam'),
+         prototype = list(RawData = new(Class = 'StQList'),
+                          EdData = new(Class = 'StQList'),
+                          VarNames = character(0),
+                          DomainNames = character(0),
+                          Imputation = new(Class = 'MeanImputationParam')),
+         validity = function(object){
+
+           if (!all(object@VarNames == object@Imputation@VarNames)) {
+
+             stop('[StQImputation:: validity ErrorProbMLEParam] The slots VarNames in the object and in the slot Imputation must be the same.\n')
+
+           }
+
+           return(TRUE)
+         }
+)
