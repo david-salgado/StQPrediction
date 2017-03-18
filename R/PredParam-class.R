2c126b60f7f49bb0dcf4ff25f0e795ea73b42408
david-salgado <david.salgado.fernandez@gmail.com>
2017-03-19 00:41:13 +0100

    Define PredTSParam class

--- R/PredParam-class.R
+++ R/PredParam-class.R
@@ -1,8 +1,8 @@
-setClassUnion('PredParam', c('PredlmParam'))
+setClassUnion('PredParam', c('PredlmParam', 'PredTSParam'))
 #' @title Virtual S4 class to handle parameters for the estimation of the probability of measurement error
 #'
 #' @name PredParam
 #'
-#' @include PredlmParam-class.R
+#' @include PredlmParam-class.R PredTSParam-class.R
 #'
 NULL
